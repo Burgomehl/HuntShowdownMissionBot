7ac7a6cf87342f65076edadd7ea450b6ba58882c
Stephan <Stephan.Maevers@gmail.com>
2018-03-02 20:39:40 +0100

    initial commit

---
+++ index.js
@@ -0,0 +1,31 @@
+const TeleBot = require('telebot');
+const bot = new TeleBot({
+    token: process.env.TELEGRAM_TOKEN, // Required. Telegram Bot API token.
+    webhook: { // Optional. Use webhook instead of polling.
+        url: process.env.APP_URL, // HTTPS url to send updates to.
+        port: process.env.PORT // Server port.
+    }
+});
+
+bot.on(['/next'], (msg) => {
+	let r = getRandomInt(4);
+	switch (r) {
+		case 0:
+			return msg.reply.text('Spinne am Tag');
+		case 1:
+			return msg.reply.text('Spinne in der Nacht');
+		case 2:
+			return msg.reply.text('Butcher am Tag');
+		case 3:
+			return msg.reply.text('Butcher in der Nacht');
+		default:
+			return msg.reply.text('Bug');
+	}
+	
+});
+
+function getRandomInt(max) {
+  return Math.floor(Math.random() * Math.floor(max));
+}
+
+bot.start();
